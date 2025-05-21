a3a501b0d5542358e30bbe95eaec61b01099ef43
OHSONGMIN <dhthdals@gmail.com>
2025-05-21 21:39:38 +0900

    Initial commit

---
+++ src/main/java/com/spring/namegen/NameGenApplication.java
@@ -0,0 +1,13 @@
+package com.spring.namegen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameGenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NameGenApplication.class, args);
+    }
+
+}
