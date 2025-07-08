4a275a5ee27fcc83328ce18ec9ba65f9743fccf3
OHSONGMIN <dhthdals@gmail.com>
2025-07-08 18:14:26 +0900

    feat: CarDTO 생성

---
+++ src/main/java/com/spring/namegen/dto/CarDTO.java
@@ -0,0 +1,27 @@
+package com.spring.namegen.dto;
+
+public class CarDTO {
+
+    private String car;
+    private String keyword;
+
+    public CarDTO() {
+    }
+
+    public String getCar() {
+        return car;
+    }
+
+    public void setCar(String car) {
+        this.car = car;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+}
