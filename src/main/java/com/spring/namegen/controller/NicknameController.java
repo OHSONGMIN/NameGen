9c919fbd154b9642861ff7a6a536188519bad9bb
OHSONGMIN <dhthdals@gmail.com>
2025-07-08 18:24:48 +0900

    feat: game, Instagram, car 닉네임 생성 Controller 추가
    - /api/nickname/game, /instagram, /car 엔드포인트 추가
    - Service 메서드 연동

--- src/main/java/com/spring/namegen/controller/NicknameController.java
+++ src/main/java/com/spring/namegen/controller/NicknameController.java
@@ -1,4 +1,47 @@
 package com.spring.namegen.controller;
 
+import com.spring.namegen.dto.CarDTO;
+import com.spring.namegen.dto.KeywordDTO;
+import com.spring.namegen.dto.ResponseDTO;
+import com.spring.namegen.service.NicknameService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/nickname")
 public class NicknameController {
+
+    private final NicknameService service;
+
+    public NicknameController(NicknameService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/game")
+    public ResponseEntity<ResponseDTO> gameNickname(@RequestBody KeywordDTO keywordDTO) {
+
+        ResponseDTO content = service.generateGameNickname(keywordDTO.getKeyword());
+        return ResponseEntity.ok(content);
+    }
+
+    @PostMapping("/instagram")
+    public ResponseEntity<ResponseDTO> instagramNickname(@RequestBody KeywordDTO keywordDTO) {
+
+        ResponseDTO content = service.generateInstagramNickname(keywordDTO.getKeyword());
+        return ResponseEntity.ok(content);
+    }
+
+    @PostMapping("/car")
+    public ResponseEntity<ResponseDTO> carNickname(@RequestBody CarDTO carDTO) {
+
+        ResponseDTO content = service.generateCarNickname(carDTO.getCar(), carDTO.getKeyword());
+        return ResponseEntity.ok(content);
+
+    }
+
 }
